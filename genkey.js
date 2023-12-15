aa6423d78dcd691de04ce4fcf164f5dac625bb57
Cordtus <Cordtus@gmail.com>
2023-12-15 06:54:51 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   genkey.js

--- genkey.js
+++ genkey.js
@@ -4,17 +4,15 @@ const bip39 = require('bip39');
 const { bech32 } = require('bech32');
 const readline = require('readline');
 
-function derivePrivateKey(mnemonic) {
+function derivePrivateKey(mnemonic, coinType) {
     // Generate a seed from the mnemonic
     const seed = bip39.mnemonicToSeedSync(mnemonic);
-    // Hash the seed to derive a private key
-    const hash = createHash('sha256').update(seed).digest();
-    let privateKey = hash;
-    // Ensure the private key is valid
-    while (!secp256k1.privateKeyVerify(privateKey)) {
-        privateKey = createHash('sha256').update(privateKey).digest();
-    }
-    return privateKey;
+    // Define the derivation path with the provided coin type
+    const hdPath = `m/44'/${coinType}'/0'/0/0`;
+    // Derive a private key based on the seed and derivation path
+    const root = bip32.fromSeed(seed);
+    const child = root.derivePath(hdPath);
+    return child.privateKey;
 }
 
 function createCosmosAddress(privateKey, prefix) {
@@ -34,13 +32,16 @@ const rl = readline.createInterface({
 });
 
 rl.question('Enter your mnemonic phrase: ', (mnemonic) => {
-    rl.question("Enter the address prefix (e.g., cosmos, chihuahua): ", (prefix) => {
-        const privateKey = derivePrivateKey(mnemonic);
-        const address = createCosmosAddress(privateKey, prefix);
+    rl.question("Enter the coin type (e.g., 118 for Cosmos): ", (coinTypeStr) => {
+        const coinType = parseInt(coinTypeStr);
+        rl.question("Enter the address prefix (e.g., cosmos, chihuahua): ", (prefix) => {
+            const privateKey = derivePrivateKey(mnemonic, coinType);
+            const address = createCosmosAddress(privateKey, prefix);
 
-        console.log(`Address: ${address}`);
-        console.log(`Private Key: ${privateKey.toString('hex')}`);
+            console.log(`Address: ${address}`);
+            console.log(`Private Key: ${privateKey.toString('hex')}`);
 
-        rl.close();
+            rl.close();
+        });
     });
 });
